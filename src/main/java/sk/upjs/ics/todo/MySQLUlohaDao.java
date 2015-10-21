3222234a6d26276823879e1f9ecb6da984fcbc50
student <student@158.197.35.90>
2015-10-21 11:24:19 +0200

    Prechod na MySQL databazu

---
+++ src/main/java/sk/upjs/ics/todo/MySQLUlohaDao.java
@@ -0,0 +1,46 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package sk.upjs.ics.todo;
+
+import com.mysql.jdbc.jdbc2.optional.MysqlDataSource;
+import java.util.List;
+import org.springframework.jdbc.core.BeanPropertyRowMapper;
+import org.springframework.jdbc.core.JdbcTemplate;
+
+/**
+ *
+ * @author student
+ */
+public class MySQLUlohaDao implements UlohaDao{
+ private JdbcTemplate jdbcTemplete;
+    public MySQLUlohaDao() {
+        MysqlDataSource dataSource= new MysqlDataSource();
+        dataSource.setUrl("jdbc>mysql//localhost/todo");
+        dataSource.setUser("Todo");
+        dataSource.setPassword("ToDo");
+        
+        jdbcTemplete= new JdbcTemplate(dataSource);
+       
+        
+        
+    }
+
+    @Override
+    public void pridat(Uloha uloha) {
+    String sql = "ÏNSERT INTO uloha VALUES(?,?,?)";
+    jdbcTemplete.update(sql, null,uloha.getNazov(),uloha.getDate());
+    }
+
+    @Override
+    public List<Uloha> dajVsetky() { 
+        
+        String sql="Select * from uloha";     
+        BeanPropertyRowMapper<Uloha> mapper= BeanPropertyRowMapper.newInstance(Uloha.class);     
+        return  jdbcTemplete.query(sql,mapper);
+       
+    }
+    
+}
